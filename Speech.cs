9576f1a37aaae4734e70f7ca4328d0a0c2a253df
Dhaval Kapil <dhavalkapil@gmail.com>
2015-02-07 12:30:38 +0530

    Added Speech.cs with functions to convert text to speech

---
+++ Speech.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace Harley
+{
+    class Speech
+    {
+        /// <summary>
+        /// Speech synthesizer object
+        /// </summary>
+        private SpeechSynthesizer speechSynthesizer;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Speech()
+        {
+            speechSynthesizer = new SpeechSynthesizer();
+        }
+
+        /// <summary>
+        /// Synchronous function to convert text to speech
+        /// </summary>
+        /// <param name="text"></param>
+        public void Speak(String text)
+        {
+            speechSynthesizer.Speak(text);
+        }
+
+        /// <summary>
+        /// Asynchronous function to convert text to speech
+        /// </summary>
+        /// <param name="text"></param>
+        public void SpeakAsync(String text)
+        {
+            speechSynthesizer.SpeakAsync(text);
+        }
+
+        /// <summary>
+        /// Function to stop existing narration
+        /// </summary>
+        public void StopSpeak()
+        {
+            if(speechSynthesizer!=null)
+            {
+                speechSynthesizer.Dispose();
+            }
+        }
+    }
+}
