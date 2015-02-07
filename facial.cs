d8c7601c70691b6d6a76bb4b8eae91759a15109f
Sopan Khosla <khoslasopan@gmail.com>
2015-02-08 04:09:14 +0530

    adds cs files for facial gesture recognition

---
+++ facial.cs
@@ -0,0 +1,192 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+
+public static class GlobalVar
+{
+    public static int Count = 0;
+
+    public static string[] result = new string[20];
+    public static int ctr = 0;
+    public static string gesture;
+    public static void maximum()
+    {
+        Trace.WriteLine("nice");
+        foreach (string line in File.ReadLines(@"C:\Users\Public\data.txt"))
+        {
+            if (ctr >= 20)
+                break;
+            result[ctr++] = line;
+        }
+
+        int[] freq = new int[5];
+
+        for (int i = 0; i < 20; i++)
+        {
+            if (result[i] == "sad")
+                freq[0]++;
+            else if (result[i] == "smiling")
+                freq[1]++;
+            else if (result[i] == "angry")
+                freq[2]++;
+            else if (result[i] == "shocked")
+                freq[3]++;
+            else freq[4]++;
+        }
+        int max = freq[0];
+        for (int i = 0; i < 4; i++)
+        {
+            if (freq[i] > max)
+                max = freq[i];
+        }
+        if (max == 0)
+            gesture = "sad";
+        else if (max == 1)
+            gesture = "smiling";
+        else if (max == 2)
+            gesture = "angry";
+        else if (max == 3)
+            gesture = "Shocked";
+        else gesture = "Not found";
+        Count = 0;
+        Array.Clear(result, 0, result.Length);
+        Trace.WriteLine("end");
+        return;
+    }
+
+}
+
+
+namespace FaceTrackingBasics
+{
+    
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly int Bgr32BytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+        private readonly KinectSensorChooser sensorChooser = new KinectSensorChooser();
+        private WriteableBitmap colorImageWritableBitmap;
+        private byte[] colorImageData;
+        private ColorImageFormat currentColorImageFormat = ColorImageFormat.Undefined;
+
+        public MainWindow()
+        {
+            Trace.WriteLine("hello");
+            InitializeComponent();
+
+            var faceTrackingViewerBinding = new Binding("Kinect") { Source = sensorChooser };
+            faceTrackingViewer.SetBinding(FaceTrackingViewer.KinectProperty, faceTrackingViewerBinding);
+
+            sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+
+            sensorChooser.Start();
+            Trace.WriteLine("hello1");
+        }
+
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs kinectChangedEventArgs)
+        {
+            KinectSensor oldSensor = kinectChangedEventArgs.OldSensor;
+            KinectSensor newSensor = kinectChangedEventArgs.NewSensor;
+            Trace.WriteLine("hello2");
+
+            if (oldSensor != null)
+            {
+                oldSensor.AllFramesReady -= KinectSensorOnAllFramesReady;
+                oldSensor.ColorStream.Disable();
+                oldSensor.DepthStream.Disable();
+                oldSensor.DepthStream.Range = DepthRange.Default;
+                oldSensor.SkeletonStream.Disable();
+                oldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                oldSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+            }
+
+            if (newSensor != null)
+            {
+                try
+                {
+                    newSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    newSensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+                    try
+                    {
+                        // This will throw on non Kinect For Windows devices.
+                        newSensor.DepthStream.Range = DepthRange.Near;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        newSensor.DepthStream.Range = DepthRange.Default;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    }
+
+                    newSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                    newSensor.SkeletonStream.Enable();
+                    newSensor.AllFramesReady += KinectSensorOnAllFramesReady;
+                    Trace.WriteLine("what");
+                }
+                catch (InvalidOperationException)
+                {
+                    // This exception can be thrown when we are trying to
+                    // enable streams on a device that has gone away.  This
+                    // can occur, say, in app shutdown scenarios when the sensor
+                    // goes away between the time it changed status and the
+                    // time we get the sensor changed notification.
+                    //
+                    // Behavior here is to just eat the exception and assume
+                    // another notification will come along if a sensor
+                    // comes back.
+                }
+            }
+        }
+
+       
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            sensorChooser.Stop();
+            faceTrackingViewer.Dispose();
+        }
+
+        private void KinectSensorOnAllFramesReady(object sender, AllFramesReadyEventArgs allFramesReadyEventArgs)
+        {
+            using (var colorImageFrame = allFramesReadyEventArgs.OpenColorImageFrame())
+            {
+                if (colorImageFrame == null)
+                {
+                    return;
+                }
+
+                // Make a copy of the color frame for displaying.
+                var haveNewFormat = this.currentColorImageFormat != colorImageFrame.Format;
+                if (haveNewFormat)
+                {
+                    this.currentColorImageFormat = colorImageFrame.Format;
+                    this.colorImageData = new byte[colorImageFrame.PixelDataLength];
+                    this.colorImageWritableBitmap = new WriteableBitmap(
+                        colorImageFrame.Width, colorImageFrame.Height, 96, 96, PixelFormats.Bgr32, null);
+                    ColorImage.Source = this.colorImageWritableBitmap;
+                }
+
+                colorImageFrame.CopyPixelDataTo(this.colorImageData);
+                this.colorImageWritableBitmap.WritePixels(
+                    new Int32Rect(0, 0, colorImageFrame.Width, colorImageFrame.Height),
+                    this.colorImageData,
+                    colorImageFrame.Width * Bgr32BytesPerPixel,
+                    0);
+            }
+        }
+    }
+}
