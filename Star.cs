8b16dd5e6f7bea60ad2585a6edee430d19611650
Dhaval Kapil <dhavalkapil@gmail.com>
2015-02-08 01:13:52 +0530

    Changed Threshold values in Star.cs

--- Star.cs
+++ Star.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Kinect;
+using System.Diagnostics;
 using KinectMeasurementsLib;
 
 namespace Harley
@@ -11,19 +12,24 @@ namespace Harley
     class Star
     {
         /// <summary>
-        /// The threshold angle at the elbow formed by the arm
+        /// The minimum threshold angle at the elbow formed by the arm
         /// </summary>
-        private const float ELBOW_ANGLE_THRESHOLD = 3.0f;
+        private const float MIN_ELBOW_ANGLE = 160.0f;
+
+        /// <summary>
+        /// The maximum threshold angle at the elbow formed by the arm
+        /// </summary>
+        private const float MAX_ELBOW_ANGLE = 190.0f;
 
         /// <summary>
         /// The minimum angle formed at the shoulder between the arm and the body
         /// </summary>
-        private const float MIN_SHOULDER_ANGLE = 117.0f;
+        private const float MIN_SHOULDER_ANGLE = 100.0f;
 
         /// <summary>
         /// The maximum angle formed at the shoulder between the arm and the body
         /// </summary>
-        private const float MAX_SHOULDER_ANGLE = 127.0f;
+        private const float MAX_SHOULDER_ANGLE = 135.0f;
 
         /// <summary>
         /// Function to check a paricular body whether it is a star or not
@@ -33,27 +39,29 @@ namespace Harley
         public static bool CheckSkeleton(Skeleton skeleton)
         {
             // Checking if left elbow is straight
-            if (KinectMeasurementsTools.AngleBetweenJoints(skeleton, JointType.ElbowLeft, JointType.WristLeft, JointType.ShoulderLeft) > ELBOW_ANGLE_THRESHOLD)
+            float leftElbowAngle = KinectMeasurementsTools.AngleBetweenJoints(skeleton, JointType.ElbowLeft, JointType.WristLeft, JointType.ShoulderLeft);
+            if ( leftElbowAngle > MAX_ELBOW_ANGLE || leftElbowAngle < MIN_ELBOW_ANGLE)
             {
                 return false;
             }
 
             // Checking if right elbow is straight
-            if (KinectMeasurementsTools.AngleBetweenJoints(skeleton, JointType.ElbowRight, JointType.WristRight, JointType.ShoulderRight) > ELBOW_ANGLE_THRESHOLD)
+            float rightElbowAngle = KinectMeasurementsTools.AngleBetweenJoints(skeleton, JointType.ElbowRight, JointType.WristRight, JointType.ShoulderRight);
+            if (rightElbowAngle > MAX_ELBOW_ANGLE || rightElbowAngle < MIN_ELBOW_ANGLE)
             {
                 return false;
             }
 
             // Checking if left should angle is correct
             float leftShoulderAngle = KinectMeasurementsTools.AngleBetweenJoints(skeleton, JointType.ShoulderLeft, JointType.HipLeft, JointType.ElbowLeft);
-            if (leftShoulderAngle<MIN_SHOULDER_ANGLE && leftShoulderAngle>MAX_SHOULDER_ANGLE)
+            if (leftShoulderAngle<MIN_SHOULDER_ANGLE || leftShoulderAngle>MAX_SHOULDER_ANGLE)
             {
                 return false;
             }
 
             // Checking if right should angle is correct
             float rightShoulderAngle = KinectMeasurementsTools.AngleBetweenJoints(skeleton, JointType.ShoulderRight, JointType.HipRight, JointType.ElbowRight);
-            if (rightShoulderAngle < MIN_SHOULDER_ANGLE && rightShoulderAngle > MAX_SHOULDER_ANGLE)
+            if (rightShoulderAngle < MIN_SHOULDER_ANGLE || rightShoulderAngle > MAX_SHOULDER_ANGLE)
             {
                 return false;
             }
