f2175fd8ead76173d15317a99bd63d9bb81bfa7e
Dhaval Kapil <dhavalkapil@gmail.com>
2015-02-08 10:45:43 +0530

    Added basic skeleton for StarActivity

---
+++ StarActivityWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Kinect.Toolbox;
+using KinectMeasurementsLib;
+
+namespace Harley
+{
+    /// <summary>
+    /// Interaction logic for StarActivityWindow.xaml
+    /// </summary>
+    public partial class StarActivityWindow : Window
+    {
+        /// <summary>
+        /// The kinect sensor object
+        /// </summary>
+        private KinectSensor kinectSensor;
+
+        /// <summary>
+        /// The speech object
+        /// </summary>
+        private Speech speech;
+
+        /// <summary>
+        /// Readonly array of word list to recognize
+        /// </summary>
+        private readonly string[] grammar = { "some", "options" };
+
+        public StarActivityWindow()
+        {
+            InitializeComponent();
+
+            InitializeKinect();
+        }
+
+        /// <summary>
+        /// Called at the start when the window is loaded
+        /// </summary>
+        private void InitializeKinect()
+        {
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.kinectSensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.kinectSensor)
+            {
+                // Turning on skeleton stream
+                this.kinectSensor.SkeletonStream.Enable();
+                this.kinectSensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+
+                this.kinectSensor.Start();
+            }
+
+            if (null == this.kinectSensor)
+            {
+                // Connection is failed
+                return;
+            }
+
+            this.speech = new Speech(this.kinectSensor, grammar);
+            this.speech.Start();
+        }
+
+        /// <summary>
+        /// Function called whenever a new skeleton frame arrives
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+                if (skeletons.Length != 0)
+                {
+                    foreach (Skeleton skel in skeletons)
+                    {
+                        if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            this.handleSkeleton(skel);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Function called whenever a skeleton is tracked
+        /// </summary>
+        /// <param name="skeleton"></param>
+        private void handleSkeleton(Skeleton skeleton)
+        {
+
+        }
+    }
+}
